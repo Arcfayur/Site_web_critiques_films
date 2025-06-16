8c01a597586b5959f6ff9ce0ad6f69715d3b7d07
Lukas SIMOES <simoes@localhost.local>
2025-06-16 21:35:12 +0200

    ajout vue erreur recherche

---
+++ ci/views/erreur_recherche.php
@@ -0,0 +1,6 @@
+<h6>Liste des séries</h6>
+<section id="recherche">
+	<!-- CSS en dur parce que picocss est insupportable vraiment -->
+	<h3 style="text-align: center; margin-top: 2em;">Aucun résultat ne correspond à votre recherche</h3>
+	<p style="text-align: center;">Essayez autre chose...</p>
+</section>
